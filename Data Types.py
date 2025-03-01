0cdfd06602d4038bc45c8cf049dbb60ed2762df4
kevin <bridget.nmaclou@gmail.com>
2025-03-01 11:28:28 +0200

    first commit

---
+++ Data Types.py
@@ -0,0 +1,15 @@
+name = input("Enter your name : ")
+print(name)
+print(type(name))
+
+age = int(input("Enter your age : "))
+print(age)
+print(type(age))
+
+weight = float(input("Enter your weight : "))
+print(weight)
+print(type(weight))
+
+isStudent = bool(input("are you a student {True/False} : "))
+print(isStudent)
+print(type(isStudent))
---
+++ string.py
@@ -0,0 +1,9 @@
+text = input("enter a string: ")
+print(f"original statement: {text}")
+
+revText = text[::-1]
+print(f"\nReverse statement: {revText}")
+
+print(f"\nUppercase: {text.upper()}")
+
+print(f"\nLowercase: {text.lower()}")
